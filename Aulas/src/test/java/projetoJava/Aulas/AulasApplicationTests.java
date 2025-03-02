78418899f7fc54a368e7cc71dc9f32f5472e405f
LuizGuilhes123 <luizguilhermeinformat@gmail.com>
2025-03-02 03:44:02 -0300

    first commit #0

---
+++ Aulas/src/test/java/projetoJava/Aulas/AulasApplicationTests.java
@@ -0,0 +1,13 @@
+package projetoJava.Aulas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
